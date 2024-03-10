e4f0e7a5bc9cd595293d23864f3213e354672b2e
BrunnoArebalor <162519797+BrunnoArebalor@users.noreply.github.com>
2024-03-10 18:03:14 -0300

    Add files via upload

---
+++ Fundamentos-de-Programa-o/Lista1/Exercicio2.py
@@ -0,0 +1,6 @@
+print('Coversor do real para dolar')
+dolar = int(input('Digite o valor que deja comprar em dolares:'))
+
+real = round(dolar * 4.98, 2)
+
+print("A conversão de $",dolar,"dolares para o real é R$",real)
