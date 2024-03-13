38ab05102df32dd108babdd805017f37abaf3968
BrunnoArebalor <brunno.arodrigues@gmail.com>
2024-03-13 20:55:31 -0300

    Delete Exercicio8.py

--- Lista1/Exercicio8.py
+++
@@ -1,7 +0,0 @@
-# 8. Um usuário deseja converter a temperatura de Celsius para Fahrenheit. Escreva um programa que
-# leia a temperatura em Celsius e exiba a temperatura equivalente em Fahrenheit.
-
-celsius = float(input("Informe um valor em celsius para converter em fahrenheit:"))
-fahrenheit = (celsius * 1.8) + 32
-
-print("O valor da conversão é:",fahrenheit,"°F")
