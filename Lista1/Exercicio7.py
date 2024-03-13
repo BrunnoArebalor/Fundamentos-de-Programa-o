d07e910b97d77a14d2851a415498d2421c33c0a8
BrunnoArebalor <brunno.arodrigues@gmail.com>
2024-03-13 20:55:40 -0300

    Delete Exercicio7.py

--- Lista1/Exercicio7.py
+++
@@ -1,9 +0,0 @@
-#7. Um criador de pássaros deseja saber a quantidade de ração diária necessária para alimentar seus
-#pássaros. Cada pássaro consome 30 gramas de ração por dia. Escreva um programa que leia o
-#número de pássaros que o criador possui e calcule a quantidade total de ração necessária por dia.
-
-passaro = int(input("Informe quantos passaros o criador tem:"))
-racao = 30
-
-totalRacao = racao * passaro
-print("O criador vai gastar",totalRacao,"gramas de ração por dia.")
