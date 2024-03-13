34f84fba5a1b955d63a1a45cac0b07ecc9e72d7e
BrunnoArebalor <brunno.arodrigues@gmail.com>
2024-03-13 20:56:41 -0300

    Delete Exercicio3.py

--- Lista1/Exercicio3.py
+++
@@ -1,8 +0,0 @@
-# 3. Um restaurante de buffet a quilo cobra R$ 40,00 por quilo.
-# Escreva um programa que leia o peso do prato do cliente e calcule o valor a ser pago.
-
-peso = int(input("Informe o peso em gramas do prato da cliente:"))
-
-preco = round(peso * 0.04, 2)
-
-print("O valor do prato da cliente ficou R$",preco)
