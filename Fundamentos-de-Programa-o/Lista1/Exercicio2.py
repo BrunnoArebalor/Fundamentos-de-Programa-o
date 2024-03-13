9ae9bf64618f6dc455b61087fd9967f69af5b288
BrunnoArebalor <brunno.arodrigues@gmail.com>
2024-03-13 20:13:22 -0300

    Delete Exercicio2.py

--- Fundamentos-de-Programa-o/Lista1/Exercicio2.py
+++
@@ -1,6 +0,0 @@
-print('Coversor do real para dolar')
-dolar = int(input('Digite o valor que deja comprar em dolares:'))
-
-real = round(dolar * 4.98, 2)
-
-print("A conversão de $",dolar,"dolares para o real é R$",real)
