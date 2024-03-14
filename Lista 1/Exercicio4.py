a0bb41430f5e8a0c5b1afb63a6e90cbd8a3a869e
BrunnoArebalor <brunno.arodrigues@gmail.com>
2024-03-13 22:13:39 -0300

    Update Exercicio4.py

--- Lista 1/Exercicio4.py
+++ Lista 1/Exercicio4.py
@@ -1,12 +1,18 @@
 print('Qual o verdadeiro nome do super-heroi Batman?:')
-print(' Opção A Clark Kent \n Opção B Bruce Wayne \n Opção C Steve Rogers \n Opção D Tony Stark \n Opção E Peter Parker')
-batman = str(input('Qual o verdadeiro nome do super-heroi Batman?:'))
 
-OpçãoA = 'A' or 'a'
-OpçãoA = 'B' or 'b'
-OpçãoA = 'C' or 'c'
-OpçãoA = 'D' or 'd'
-OpçãoA = 'E' or 'e'
+OpçãoA = 'a) Bruce Wayne'
+OpçãoB = 'b) Clark Kent'
+OpçãoC = 'c) Peter Parker'
+OpçãoD = 'd) Tony Stark'
+OpçãoE = 'e) Steve Rogers'
+
+print(OpçãoA)
+print(OpçãoB)
+print(OpçãoC)
+print(OpçãoD)
+print(OpçãoE)
+
+batman = str(input('Qual o verdadeiro nome do super-heroi Batman?:'))
 
 if batman == 'B' or batman == 'b':
     print('certo')
