d6abfb7ed9ee3b37c9f4fb9cc99a9c32bc8fb376
BrunnoArebalor <brunno.arodrigues@gmail.com>
2024-03-13 21:23:25 -0300

    Create Exerccio3.py

---
+++ Lista 1/Exerccio3.py
@@ -0,0 +1,9 @@
+print('Qual o verdadeiro nome do super-heroi Batman?:')
+print(' Opção A Clark Kent \n Opção B Bruce Wayne \n Opção C Steve Rogers \n Opção D Tony Stark \n Opção E Peter Parker')
+batman = str(input('Qual o verdadeiro nome do super-heroi Batman?:'))
+
+
+if batman == 'B':
+    print('certo')
+elif batman != 'B':
+    print('errado')
